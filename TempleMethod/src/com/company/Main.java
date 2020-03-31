ffeec66682c8e6818f50f2a4ad84ca1636cbf09f
Benjamin Bowo <ben.bowo@gmail.com>
2020-03-31 16:02:47 +0300

    TemplateMethod Push

---
+++ TempleMethod/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RistiNolla ristiNolla = new RistiNolla();
+        ristiNolla.playOneGame(2);
+    }
+}
